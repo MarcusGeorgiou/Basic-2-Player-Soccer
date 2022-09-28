ad017769bb676f80c1c0f75e303acb466b55736e
DESKTOP-4RB7EDR\voula <1027503@student.sae.edu.au>
2022-09-28 16:59:55 +1000

    I broke the movement

--- Assets/RedPlayer.cs
+++ Assets/RedPlayer.cs
@@ -1,33 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RedPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Teams myTeam;
+
+    private Rigidbody rb;
+
+    private void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey("w"))
-		{
-			this.transform.position += new Vector3(0, 0, 0.05f);
-		}
-		else if(Input.GetKey("a"))
-		{
-			this.transform.position += new Vector3(-0.05f, 0, 0);
-		}
-		else if(Input.GetKey("s"))
-		{
-			this.transform.position += new Vector3(0, 0, -0.05f);
-		}
-		else if(Input.GetKey("d"))
-		{
-			this.transform.position += new Vector3(0.05f, 0, 0);
-		}
+        if (myTeam == Teams.Red)
+        {
+            Vector3 redMove = new Vector3(Input.GetAxis("HorizRed"), 0, Input.GetAxis("VertRed"));
+            rb.AddForce(redMove * 0.02f * Time.deltaTime);
+        }
+
+        if (myTeam == Teams.Blue)
+        {
+            Vector3 blueMove = new Vector3(Input.GetAxis("HorizBlue"), 0, Input.GetAxis("VertBlue"));
+            rb.AddForce(blueMove * 0.02f * Time.deltaTime);
+        }
     }
 }

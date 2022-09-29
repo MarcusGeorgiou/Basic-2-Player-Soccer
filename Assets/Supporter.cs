8c7ba4b5dcb64e8f0395dff12ba6db39e8cf3b84
DESKTOP-4RB7EDR\voula <1027503@student.sae.edu.au>
2022-09-29 13:24:17 +1000

    Passive Supporters

---
+++ Assets/Supporter.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Supporter : MonoBehaviour
+{
+	private Teams myTeam;
+	float jumpTimer = 0.2f;
+	
+	private Rigidbody rb;
+	private MeshRenderer mr;
+	public Material[] teams;
+	
+	private void OnEnable()
+	{
+		
+	}
+
+	private void Awake()
+	{
+		if (Random.Range(0, 2) == 0)
+		{
+			myTeam = Teams.Red;
+		}
+		else
+		{
+			myTeam = Teams.Blue;
+		}
+	}
+
+	// Start is called before the first frame update
+    void Start()
+    {
+	    rb = this.GetComponent<Rigidbody>();
+	    mr = this.GetComponent<MeshRenderer>();
+
+	    if (myTeam == Teams.Red)
+	    {
+		    mr.material = teams[1];
+	    }
+	    else if (myTeam == Teams.Blue)
+	    {
+		    mr.material = teams[0];
+	    }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    jumpTimer -= Time.deltaTime;	    
+	    
+	    if (Random.Range(0, 10) == 5 && jumpTimer <= 0)
+	    {
+		    jumpTimer = 1.1f;
+		    rb.AddForce(0, Random.Range(50, 120), 0);
+	    }
+    }
+}

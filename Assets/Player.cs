114b125ba841385fb9b65db279f91bec8bef475a
MarcusGeorgiou <1027503@student.sae.edu.au>
2022-10-14 12:34:08 +1100

    well this didn't work.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -13,6 +13,9 @@ public class Player : MonoBehaviour
     
     Soccer controls;
 
+    private Vector2 redMove;
+    private Vector2 blueMove;
+
     void Awake()
     {
         controls = new Soccer();
@@ -29,26 +32,34 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*if (myTeam == Teams.Red)
+        if (myTeam == Teams.Red)
         {
-            Vector3 redMove = new Vector3(Input.GetAxis("HorizRed"), 0, Input.GetAxis("VertRed"));
+            //Vector3 redMove = new Vector3(Input.GetAxis("HorizRed"), 0, Input.GetAxis("VertRed"));
+            Vector3 rVelocity = new Vector3(redMove.x, 0, redMove.y);
             rb.AddForce(redMove * speed * Time.deltaTime);
-            
         }
 
         if (myTeam == Teams.Blue)
         {
-            Vector3 blueMove = new Vector3(Input.GetAxis("HorizBlue"), 0, Input.GetAxis("VertBlue"));
-            rb.AddForce(blueMove * speed * Time.deltaTime);
+            //Vector3 blueMove = new Vector3(Input.GetAxis("HorizBlue"), 0, Input.GetAxis("VertBlue"));
+            Vector3 bVelocity = new Vector3(blueMove.x, 0, blueMove.y);
+            rb.AddForce(bVelocity * speed * Time.deltaTime);
         }
-        */
+        
     }
     
     void MovePlayer(InputAction.CallbackContext input)
     {
         print("Moved");
         print(input);
-        
-        //Something to move players on x and z axes
+
+        if (myTeam == Teams.Blue)
+        {
+            blueMove = input.ReadValue<Vector2>();
+        }
+        else if (myTeam == Teams.Red)
+        {
+            redMove = input.ReadValue<Vector2>();
+        }
     }
 }
